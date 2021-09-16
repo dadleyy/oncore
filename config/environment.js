562610d3f50f3a1eaea11e934f5684e1ce930483
Danny Hadley <dadleyy@gmail.com>
2021-09-16 17:36:34 -0400

    remove some stuff, do pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'oncore',
+    podModulePrefix: 'oncore/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
